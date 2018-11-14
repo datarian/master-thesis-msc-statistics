81a5af4dd3887c6c6da274f2cf3e2c33ddaa4f30
florian <contact@againstthecurrent.ch>
2018-11-14 16:51:34 +0100

    bugfix

--- code/kdd98/transformers.py
+++ code/kdd98/transformers.py
@@ -1143,7 +1143,7 @@ class OrdinalEncoder(BaseEstimator, TransformerMixin):
         self.mapping = categories
 
         for switch in self.mapping:
-            self.feature_names.append([switch.get('col')])
+            self.feature_names.append(switch.get('col'))
 
         # drop all output columns with 0 variance.
         if self.drop_invariant:
