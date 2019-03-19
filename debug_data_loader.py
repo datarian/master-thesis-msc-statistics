0035657de0cc7e8b617a4871ff9f49f9ccec7810
florian <datarian@againstthecurrent.ch>
2019-03-19 14:06:25 +0100

    Check in remaining changes for release

--- debug_data_loader.py
+++ debug_data_loader.py
@@ -1,12 +1,21 @@
-import kdd98.data_loader as dl
-from kdd98.data_loader import Cleaner
-from kdd98.config import Config
-from kdd98.transformers import DeltaTime, MonthsToDonation
-from sklearn.compose import ColumnTransformer
+import logging
+import kdd98.data_handler as dh
+from kdd98.transformers import ZeroVarianceSparseDropper
 
+# Set up the logger
+logging.basicConfig(filename=__name__ + '.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-provider = dl.KDD98DataLoader("cup98LRN_snip.txt")
+provider = dh.KDD98DataProvider("cup98LRN_snip.txt")
 
-preprocessed = provider.preprocessed_data
+numeric = provider.numeric_data
+# clean = provider.clean_data
 
-print(preprocessed.info())
+# a = set()
+# for i in range(3, 25):
+#     aa = clean.loc[:, "RFA_" + str(i)].tolist()
+#     for r in aa:
+#         if not isinstance(r, float):
+#             if len(r) == 3:
+#                 a.add(r[2])
+# print(a)
