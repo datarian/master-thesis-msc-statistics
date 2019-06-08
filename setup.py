3ba49784716f07bd3aafd0c5ebb944c94ff6f0bd
florian <datarian@againstthecurrent.ch>
2019-06-08 13:19:01 +0000

    bumped version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from distutils.core import setup
 import distutils.command.bdist_conda
 
-__version__ = '0.4'
+__version__ = '0.5'
 
 # Get the long description from the README file
 with open('README.md') as f:
